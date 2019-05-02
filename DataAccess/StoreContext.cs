24bf5235d7a667a84958827bb59d4fd92aeacaee
gustavomirandag <gustavomirandag@gmail.com>
2019-05-02 08:49:41 -0300

    Simple .NET Core MVC App

---
+++ DataAccess/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using DomainModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public StoreContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=tcp:awesomestoreserver.database.windows.net,1433;Initial Catalog=awesomestoredb;Persist Security Info=False;User ID=classbrothers;Password=@dsInf123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
---
+++ DomainModel/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DomainModel
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
