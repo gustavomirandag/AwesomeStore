cb28b214cff9cbc18a0c1efadb06af92707e300c
gustavomirandag <gustavomirandag@gmail.com>
2019-07-27 13:59:17 -0300

    Just a test

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+//sldkfjdslfd
+
 namespace WebApp
 {
     public class Startup
